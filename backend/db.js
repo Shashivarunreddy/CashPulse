ee9212be6cb3a15a926a1443c45f09c47b1a3d4a
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-02-26 17:39:14 +0530

    verified errors

--- backend/db.js
+++ backend/db.js
@@ -2,31 +2,29 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/paytm-clone");
 const userSchema = new mongoose.Schema({
-  username: {
+  userName: {
       type: String,
       required: true,
       unique: true,
       trim: true,
       lowercase: true,
-      minLength: 3,
-      maxLength: 30
+
   },
   password: {
       type: String,
       required: true,
-      minLength: 6
+  
   },
   firstName: {
       type: String,
       required: true,
       trim: true,
-      maxLength: 50
+   
   },
   lastName: {
       type: String,
       required: true,
       trim: true,
-      maxLength: 50
   }
 });
 
