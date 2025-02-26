e929e738bdaa3463988b7340e052de33c4ddaf24
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-02-26 13:18:45 +0530

    added some components

---
+++ frontend/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <div className='font-bold text-4xl pt-6'>
+      {label}
+    </div>
+  )
+}
+
+export default Heading
---
+++ frontend/components/InputBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function InputBox({label, placeholder}) {
+  return (
+    <div>
+      <div className='text-sm font-medium text-left py-2'>
+        {label}
+      </div>
+      <input
+        className='w-full px-2 py-1 border rounded border-slate-200'
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
+
+export default InputBox
